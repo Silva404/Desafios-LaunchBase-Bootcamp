4740002299c394677fa00da62a0e1a5e807cb8b4
Silva404 <brenosilva0252@gmail.com>
2020-06-20 16:51:13 -0300

    semana 01, completada

--- operacoes-bancarias.js
+++ operacoes-bancarias.js
@@ -6,21 +6,57 @@ const user = {
     balance: 0
 };
 
+
+// adicionar transações
 function createTransaction(transaction) {
-    transaction = {
-        type: nType,
-        value: 50.5
-    }
-
-    if (nType == 'credit') {
-        user.balance += credit
-    } else if (nType == 'debit') {
-        user.balance = debit
-    }
-
-    
-
     user.transactions.push(transaction)
+
+    if (transaction.type == 'credit') {
+        user.balance += transaction.value
+    } else {
+        user.balance -= transaction.value
+    }
 }
 
-console.log(createTransaction(user))
+// relatórios
+function getHigherTransactionByType(type) {
+    let higherTransaction
+    let higherValue = 0
+
+    for (let transaction of user.transactions) {
+        if (transaction.type == type && transaction.value > higherValue) {
+            higherTransaction = transaction.value
+            higherTransaction = transaction
+        }
+    }
+
+    return higherTransaction
+}
+
+function getAverageTransactionValue() {
+    let sum = 0
+
+    for (let transaction of user.transactions) {
+        sum += transaction.value
+    }
+
+    return sum / user.transactions.length
+}
+
+function getTransactionsCount() {
+    let count = {
+        credit: 0,
+        debit: 0
+    }
+
+
+    for (let transaction of user.transactions) {
+        if (transaction.type == 'credit') {
+            count.credit++
+        } else {
+            count.debit++
+        }
+    }
+
+    return count
+}
